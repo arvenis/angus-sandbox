c24e818fe78814a55300af4d2ad6b26287e8b0ab
Istvan Banfi <ibanfi@gmail.com>
2020-04-22 12:14:43 +0200

    add node, fix ca name

--- res/update-files.sh
+++ res/update-files.sh
@@ -10,7 +10,12 @@ cp -r /root/.minikube /home/vagrant
 # Set prompt
 echo "export PS1=\"\[\e[1;91m\]\h:\W $\[\e[0m\] \"" >> .bashrc
 echo "alias fcli='kubectl exec -it fabric-cli --'" >> .bashrc
-echo "alias start-minikube='sudo minikube start --vm-driver=none --apiserver-ips 127.0.0.1 --apiserver-name localhost'"
+echo "alias start-minikube='sudo minikube start --vm-driver=none --apiserver-ips 127.0.0.1 --apiserver-name localhost'" >> .bashrc
+
+# Set internal HLF nodes
+echo "10.101.10.1     ca" >> /etc/hosts
+echo "10.101.10.2     orderer" >> /etc/hosts
+echo "10.101.10.3     peer0" >> /etc/hosts
 
 # Set LANG environments
 echo "LANG=en_US.utf-8" > /etc/environment
